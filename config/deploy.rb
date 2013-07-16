7172f1fff93d8e99250945b6b3e59990b7131e28
Roy Young <zhuoyuyang@gmail.com>
2013-07-16 23:39:54 +0800

    Fix deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,7 @@ require "bundler/capistrano"
 require "sidekiq/capistrano"
 
 default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
 
 set :application, "community"
 set :repository,  "git@github.com:poiyzy/ruby-china.git"
@@ -15,10 +16,9 @@ else
   set :deploy_to, "/home/#{user}/apps/#{application}"
 end
 set :deploy_via, :remote_cache
-set :runner, "poiyzy"
+# set :runner, "poiyzy"
 set :use_sudo, false
-# set :deploy_via, :remote_cache
-set :git_shallow_clone, 1
+# set :git_shallow_clone, 1
 
 role :web, "42.121.111.183"                          # Your HTTP server, Apache/etc
 role :app, "42.121.111.183"                          # This may be the same as your `Web` server
@@ -60,34 +60,35 @@ namespace :faye do
 end
 
 
-task :init_shared_path, :roles => :web do
+task :init_shared_path, :roles => :app do
   run "mkdir -p #{deploy_to}/shared/log"
   run "mkdir -p #{deploy_to}/shared/pids"
   run "mkdir -p #{deploy_to}/shared/assets"
 end
 
-task :link_shared_files, :roles => :web do
+task :link_shared_files, :roles => :app do
   run "ln -sf #{deploy_to}/shared/config/*.yml #{deploy_to}/current/config/"
   run "ln -sf #{deploy_to}/shared/config/unicorn.rb #{deploy_to}/current/config/"
   run "ln -sf #{deploy_to}/shared/config/initializers/secret_token.rb #{deploy_to}/current/config/initializers"
   run "ln -sf #{deploy_to}/shared/config/faye_thin.yml #{deploy_to}/current/faye_server/thin.yml"
-  run "ln -nfs #{deploy_to}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+  sudo "ln -nfs #{deploy_to}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
 end
 
-task :mongoid_create_indexes, :roles => :web do
+task :mongoid_create_indexes, :roles => :app do
   run "cd #{deploy_to}/current/; RAILS_ENV=production bundle exec rake db:mongoid:create_indexes"
 end
 
-task :compile_assets, :roles => :web do
+task :compile_assets, :roles => :app do
   run "cd #{deploy_to}/current/; RAILS_ENV=production bundle exec rake assets:precompile"
 end
 
-task :sync_assets_to_cdn, :roles => :web do
-  run "cd #{deploy_to}/current/; RAILS_ENV=production bundle exec rake assets:cdn"
-end
+# task :sync_assets_to_cdn, :roles => :web do
+#   run "cd #{deploy_to}/current/; RAILS_ENV=production bundle exec rake assets:cdn"
+# end
 
-task :mongoid_migrate_database, :roles => :web do
+task :mongoid_migrate_database, :roles => :app do
   run "cd #{deploy_to}/current/; RAILS_ENV=production bundle exec rake db:migrate"
 end
 
-after "deploy:finalize_update","deploy:symlink", :init_shared_path, :link_shared_files, :compile_assets, :sync_assets_to_cdn, :mongoid_migrate_database
+# after "deploy:finalize_update","deploy:symlink", :init_shared_path, :link_shared_files, :compile_assets, :sync_assets_to_cdn, :mongoid_migrate_database
+after "deploy:finalize_update","deploy:symlink", :init_shared_path, :link_shared_files, :compile_assets, :mongoid_migrate_database
